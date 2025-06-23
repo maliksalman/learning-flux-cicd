64940ca6fd0bb3c24ccb1fd44d99ed20543c630f
Salman Malik <salman.the.malik@gmail.com>
2025-06-23 11:29:43 -0400

    maving the kusotmization objects to the apps directory

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CLUSTER_NAME=$1
+if [[ -z $CLUSTER_NAME || ! -d clusters/${CLUSTER_NAME}/flux-system ]]; then
+	echo "Error: {CLUSTER_NAME} is required and a matching clusters/{CLUSTER_NAME}/flux-system directory should exist"
+	echo "Usage: $0 {CLUSTER_NAME}
+	exit 1
+fi
+
+kubectl kustomize clusters/${CLUSTER_NAME}/flux-system | kubectl apply -f -
+sleep 10
+kubectl kustomize clusters/${CLUSTER_NAME}/flux-system | kubectl apply -f -
+sleep 10
+
+flux create secret git flux-system \
+	--url=ssh://git@github.com/maliksalman/learning-flux-cicd \
+	--private-key-file=$HOME/.ssh/id_ed25519
